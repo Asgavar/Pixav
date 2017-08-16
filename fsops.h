10a2a6e318a3d7c1d78c45738245b79c90ebcca4
Artur Juraszek <asgavar@gmail.com>
2017-08-16 11:54:32 +0200

    Add double inclusion guards

--- fsops.h
+++ fsops.h
@@ -1,2 +1,7 @@
+#ifndef FSOPS_H
+#define FSOPS_H
+
 char* basename(char*);
 char* temp_folder_name(char*);
+
+#endif
--- vidops.h
+++ vidops.h
@@ -1,1 +1,6 @@
+#ifndef VIDOPS_H
+#define VIDOPS_H
+
 void split_to_frames(char*, char*);
+
+#endif
