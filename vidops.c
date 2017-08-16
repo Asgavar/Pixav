abf5fc3f025e88aa54a6e31d57829f1883b71029
Artur Juraszek <asgavar@gmail.com>
2017-08-16 21:49:00 +0200

    Tune ffmpeg invoking command

--- vidops.c
+++ vidops.c
@@ -7,6 +7,6 @@
 void split_to_frames(char* path_to_file, char* temp_folder) {
     char* ffmpeg_command = malloc(2048);
     /* -i stands for input, output is provided afterwards */
-    sprintf(ffmpeg_command, "ffmpeg -i %s %s/frame%%04d.jpg", path_to_file, temp_folder);
+    sprintf(ffmpeg_command, "ffmpeg -i %s -qscale:v 0 %s/frame%%06d.jpg", path_to_file, temp_folder);
     system(ffmpeg_command);
 }
